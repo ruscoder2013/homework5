f7caa49aa6fd5095a9430d2ea6f20095c5c62351
ruscoder2013 <gusev-a@mail.ru>
2021-03-05 00:19:30 +0300

    update main

--- main.cpp
+++ main.cpp
@@ -9,10 +9,7 @@ public:
     {
         friend class Matrix;
     public:
-        IntAdapter(Matrix &parent, int row, int col) : 
-            parent(parent),
-            row(row),
-            col(col){ }
+        IntAdapter(Matrix &parent, int row, int col) : parent(parent), row(row), col(col){ }
         operator int() { 
             if ( parent.mat.find(row) != parent.mat.end() )
                 if ( parent.mat[row].find(col) != parent.mat[row].end())
@@ -22,16 +19,16 @@ public:
         int& operator =(const int &a) 
         {
             if (a == default_number)
-             {
-                  if ( parent.mat.find(row) != parent.mat.end() )
-                 {
-                     if ( parent.mat[row].find(col) != parent.mat[row].end()) {
-                         parent.mat[row].erase(col);
-                         if (parent.mat[row].size() == 0)
+            {
+                if ( parent.mat.find(row) != parent.mat.end() )
+                {
+                    if ( parent.mat[row].find(col) != parent.mat[row].end()) {
+                        parent.mat[row].erase(col);
+                        if (parent.mat[row].size() == 0)
                             parent.mat.erase(row);
-                     }
+                    }
                 }
-             }
+            }
             else
                 parent.mat[row][col] = a;
             return (int&)a;
@@ -44,11 +41,8 @@ public:
     class RowProxy {
         friend class Matrix;
     public:
-        RowProxy(Matrix &parent, int row) : 
-            parent(parent),
-            row(row)
+        RowProxy(Matrix &parent, int row) : parent(parent), row(row)
         { }
-        
         IntAdapter operator[](int col)
         {
             return IntAdapter(parent, row, col);
@@ -58,55 +52,50 @@ public:
         int row;
     };
     class iterator
+    {
+    	friend class Matrix; 
+    public:
+        iterator(Matrix &parent, std::map<int, std::map<int, int>>::iterator it_1, std::map<int,int>::iterator it_2) : parent(parent), 
+	it(it_1), it2(it_2) {}
+	iterator& operator=(const iterator &iter)
 	{
-		friend class Matrix; 
-	public:
- 		iterator(Matrix &parent, 
- 		 std::map<int, std::map<int, int>>::iterator it_1,
-	    std::map<int,int>::iterator it_2
-	    ) :parent(parent), it(it_1), it2(it_2) {
+            it = iter.it;
+            it2 = iter.it2;
+	    return *this;
+	}
+	bool operator == (const iterator &iter) const
+	{
+	    if(it != parent.mat.end())
+	        return ((iter.it == it) && (iter.it2 == it2));
+	    else 
+                return (iter.it == it);
+	}
+	bool operator!=(const iterator &it) const
+	{
+	    return !(it == *this);
+	}
+	iterator& operator++()
+	{
+            it2 = ++it2;
+            if(it2 == it->second.end()) {
+                it++;
+		if(it != parent.mat.end())
+		    it2 = it->second.begin();
 	    }
-		iterator& operator=(const iterator &iter)
-		{
-		    it = iter.it;
-		    it2 = iter.it2;
-			return *this;
-		}
-		bool operator == (const iterator &iter) const
-		{
-		    if(it != parent.mat.end())
-			    return ((iter.it == it) && (iter.it2 == it2));
-			else 
-			    return (iter.it == it);
-		}
-		bool operator!=(const iterator &it) const
-		{
-			return !(it == *this);
-		}
-		iterator& operator++()
-		{
-		    it2 = ++it2;
-		    if(it2 == it->second.end()) {
-		        it++;
-		        if(it != parent.mat.end())
-		            it2 = it->second.begin();
-		    }
             return *this;
-		}		
-		std::tuple<int, int, int> operator*() const
-		{
+        }		
+	std::tuple<int, int, int> operator*() const
+	{
             int a = it->first;
             int b = it2->first;
             int c = it2->second;
             return std::make_tuple(a, b, c);
-		}
-
+	}
 	private:
 	    std::map<int, std::map<int, int>>::iterator it;
 	    std::map<int,int>::iterator it2;
 	    Matrix& parent;
-	};
-	
+	};	
     int size() {
         int size = 0;
         for(const auto& row : mat)
